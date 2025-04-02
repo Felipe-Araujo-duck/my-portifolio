d6903720865a70ca5212e021d952c9ef71ea2989
Felipe <fellipearraujjo2004@gmail.com>
2025-04-01 21:42:50 -0300

    style: novo component

---
+++ src/pages/Home/sections/About/About.tsx
@@ -0,0 +1,13 @@
+import { Typography } from "@mui/material"
+
+const About = () => {
+
+    return (
+      <>
+        <Typography>Teste</Typography>        
+      </>
+    )
+  }
+  
+  export default About
+  
